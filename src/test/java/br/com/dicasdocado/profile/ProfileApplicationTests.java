c335c0729abdaab1c325bc3b98244a88ebb7d565
Ricardo Alcantara <ricardo.alcantara.silva@gmail.com>
2024-08-02 21:21:09 -0300

    Feat: Basic project created

---
+++ src/test/java/br/com/dicasdocado/profile/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dicasdocado.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
